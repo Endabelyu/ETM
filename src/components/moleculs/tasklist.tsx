b2f521dc09b31448c21b6bb3f5640c2cc338d962
naswa <endabelyuproject@gmail.com>
2024-07-31 19:47:16 +0700

    feat(taskList): separate tasklist component from app tsx

---
+++ src/components/moleculs/tasklist.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Task } from '@/App';
+
+const Tasklist = ({ taskData }: { taskData: Task[] }) => {
+  return (
+    <>
+      {taskData.length > 0 ? (
+        taskData.map(({ id, name, priority, status }) => (
+          <Card key={id}>
+            <CardHeader>
+              <CardTitle>{name}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription>{priority}</CardDescription>
+              <CardDescription>{status}</CardDescription>
+            </CardContent>
+          </Card>
+        ))
+      ) : (
+        <Card>
+          <CardContent>
+            <CardDescription>Task is empty</CardDescription>
+          </CardContent>
+        </Card>
+      )}
+    </>
+  );
+};
+
+export default Tasklist;
