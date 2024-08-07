5f581f966bf6ec8adefca0fab0330d1aedf731fe
naswa <endabelyuproject@gmail.com>
2024-08-07 19:47:42 +0700

    feat(searchbar): create new searchbar component

--- src/components/molecules/searchbar.tsx
+++ src/components/molecules/searchbar.tsx
@@ -1,16 +1,16 @@
-import { Input } from '../ui/input';
+import { Input } from "../ui/input";
 
 type searchProps = {
   setSearch: (searchValue: string) => void;
 };
 const Searchbar = ({ setSearch }: searchProps) => {
   return (
-    <div className='tailwind.config.jsflex tailwind.config.jsmin-w-1/2  tailwind.config.jsmax-w-sm tailwind.config.jsitems-center tailwind.config.jsgap-2 tailwind.config.jsspace-x-2'>
+    <div className="flex min-w-full items-center gap-2 space-x-2">
       <Input
-        id='name'
-        name='name'
-        placeholder='Search Task'
-        className=''
+        id="name"
+        name="name"
+        placeholder="Search Task"
+        className=""
         onChange={(e) => setSearch(e.target.value)}
       />
     </div>
