2d9db23d640c71dfc00f60d9ddba83bbee97e6fd
naswa <endabelyuproject@gmail.com>
2024-08-07 19:43:28 +0700

    feat(baseLayout): add some component for frequesntly used

--- src/components/organism/baseLayout.tsx
+++ src/components/organism/baseLayout.tsx
@@ -1,11 +1,21 @@
-import React, { Children } from 'react';
-import Navbar from '../molecules/navbar';
-
-const BaseLayout = ({ children }: { children: React.ReactNode }) => {
+import React from "react";
+import Navbar from "@/components/molecules/navbar";
+import Sidebar from "../molecules/sidebar";
+type baseLayoutProps = {
+  children: React.ReactNode;
+  setSearchValue: (search: string) => void;
+};
+const BaseLayout = ({
+  children,
+  setSearchValue: searchValue,
+}: baseLayoutProps) => {
   return (
-    <div>
-      <Navbar />
-      {children}
+    <div className="flex flex-col">
+      <Navbar setSearchValue={searchValue} />
+      <div className="flex gap-8">
+        <Sidebar />
+        {children}
+      </div>
     </div>
   );
 };
