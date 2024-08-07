bf1a62e5235da5ae2be69971509c67e382d406cf
naswa <endabelyuproject@gmail.com>
2024-08-07 19:49:10 +0700

    feat(navigation-menu): import navigation-menu  component

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -1,9 +1,9 @@
-import * as React from "react"
-import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu"
-import { cva } from "class-variance-authority"
-import { ChevronDown } from "lucide-react"
+import * as React from "react";
+import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
+import { cva } from "class-variance-authority";
+import { ChevronDown } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const NavigationMenu = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Root>,
@@ -11,17 +11,17 @@ const NavigationMenu = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <NavigationMenuPrimitive.Root
     ref={ref}
+    id="root-menu"
     className={cn(
-      "tailwind.config.jsrelative tailwind.config.jsz-10 tailwind.config.jsflex tailwind.config.jsmax-w-max tailwind.config.jsflex-1 tailwind.config.jsitems-center tailwind.config.jsjustify-center",
-      className
+      "relative z-10 flex max-w-full flex-1 items-center justify-center",
+      className,
     )}
     {...props}
   >
     {children}
-    <NavigationMenuViewport />
   </NavigationMenuPrimitive.Root>
-))
-NavigationMenu.displayName = NavigationMenuPrimitive.Root.displayName
+));
+NavigationMenu.displayName = NavigationMenuPrimitive.Root.displayName;
 
 const NavigationMenuList = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.List>,
@@ -30,19 +30,19 @@ const NavigationMenuList = React.forwardRef<
   <NavigationMenuPrimitive.List
     ref={ref}
     className={cn(
-      "tailwind.config.jsgroup tailwind.config.jsflex tailwind.config.jsflex-1 tailwind.config.jslist-none tailwind.config.jsitems-center tailwind.config.jsjustify-center tailwind.config.jsspace-x-1",
-      className
+      "group flex flex-1 list-none items-center justify-between space-x-1",
+      className,
     )}
     {...props}
   />
-))
-NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName
+));
+NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 
-const NavigationMenuItem = NavigationMenuPrimitive.Item
+const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva(
-  "tailwind.config.jsgroup tailwind.config.jsinline-flex tailwind.config.jsh-10 tailwind.config.jsw-max tailwind.config.jsitems-center tailwind.config.jsjustify-center tailwind.config.jsrounded-md tailwind.config.jsbg-background tailwind.config.jspx-4 tailwind.config.jspy-2 tailwind.config.jstext-sm tailwind.config.jsfont-medium tailwind.config.jstransition-colors hover:tailwind.config.jsbg-accent hover:tailwind.config.jstext-accent-foreground focus:tailwind.config.jsbg-accent focus:tailwind.config.jstext-accent-foreground focus:tailwind.config.jsoutline-none disabled:tailwind.config.jspointer-events-none disabled:tailwind.config.jsopacity-50 data-[active]:tailwind.config.jsbg-accent/50 data-[state=open]:tailwind.config.jsbg-accent/50"
-)
+  "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50",
+);
 
 const NavigationMenuTrigger = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Trigger>,
@@ -50,17 +50,17 @@ const NavigationMenuTrigger = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <NavigationMenuPrimitive.Trigger
     ref={ref}
-    className={cn(navigationMenuTriggerStyle(), "tailwind.config.jsgroup", className)}
+    className={cn(navigationMenuTriggerStyle(), "group", className)}
     {...props}
   >
     {children}{" "}
     <ChevronDown
-      className="tailwind.config.jsrelative tailwind.config.jstop-[1px] tailwind.config.jsml-1 tailwind.config.jsh-3 tailwind.config.jsw-3 tailwind.config.jstransition tailwind.config.jsduration-200 group-data-[state=open]:tailwind.config.jsrotate-180"
+      className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
       aria-hidden="true"
     />
   </NavigationMenuPrimitive.Trigger>
-))
-NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName
+));
+NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName;
 
 const NavigationMenuContent = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Content>,
@@ -69,33 +69,33 @@ const NavigationMenuContent = React.forwardRef<
   <NavigationMenuPrimitive.Content
     ref={ref}
     className={cn(
-      "tailwind.config.jsleft-0 tailwind.config.jstop-0 tailwind.config.jsw-full data-[motion^=from-]:tailwind.config.jsanimate-in data-[motion^=to-]:tailwind.config.jsanimate-out data-[motion^=from-]:tailwind.config.jsfade-in data-[motion^=to-]:tailwind.config.jsfade-out data-[motion=from-end]:tailwind.config.jsslide-in-from-right-52 data-[motion=from-start]:tailwind.config.jsslide-in-from-left-52 data-[motion=to-end]:tailwind.config.jsslide-out-to-right-52 data-[motion=to-start]:tailwind.config.jsslide-out-to-left-52 md:tailwind.config.jsabsolute md:tailwind.config.jsw-auto tailwind.config.js",
-      className
+      "left-0 top-0 w-full data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-52 data-[motion=from-start]:slide-in-from-left-52 data-[motion=to-end]:slide-out-to-right-52 data-[motion=to-start]:slide-out-to-left-52 md:absolute md:w-auto",
+      className,
     )}
     {...props}
   />
-))
-NavigationMenuContent.displayName = NavigationMenuPrimitive.Content.displayName
+));
+NavigationMenuContent.displayName = NavigationMenuPrimitive.Content.displayName;
 
-const NavigationMenuLink = NavigationMenuPrimitive.Link
+const NavigationMenuLink = NavigationMenuPrimitive.Link;
 
 const NavigationMenuViewport = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Viewport>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Viewport>
 >(({ className, ...props }, ref) => (
-  <div className={cn("tailwind.config.jsabsolute tailwind.config.jsleft-0 tailwind.config.jstop-full tailwind.config.jsflex tailwind.config.jsjustify-center")}>
+  <div className={cn("absolute left-0 top-full flex justify-center")}>
     <NavigationMenuPrimitive.Viewport
       className={cn(
-        "tailwind.config.jsorigin-top-center tailwind.config.jsrelative tailwind.config.jsmt-1.5 tailwind.config.jsh-[var(--radix-navigation-menu-viewport-height)] tailwind.config.jsw-full tailwind.config.jsoverflow-hidden tailwind.config.jsrounded-md tailwind.config.jsborder tailwind.config.jsbg-popover tailwind.config.jstext-popover-foreground tailwind.config.jsshadow-lg data-[state=open]:tailwind.config.jsanimate-in data-[state=closed]:tailwind.config.jsanimate-out data-[state=closed]:tailwind.config.jszoom-out-95 data-[state=open]:tailwind.config.jszoom-in-90 md:tailwind.config.jsw-[var(--radix-navigation-menu-viewport-width)]",
-        className
+        "origin-top-center mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]",
+        className,
       )}
       ref={ref}
       {...props}
     />
   </div>
-))
+));
 NavigationMenuViewport.displayName =
-  NavigationMenuPrimitive.Viewport.displayName
+  NavigationMenuPrimitive.Viewport.displayName;
 
 const NavigationMenuIndicator = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Indicator>,
@@ -104,16 +104,16 @@ const NavigationMenuIndicator = React.forwardRef<
   <NavigationMenuPrimitive.Indicator
     ref={ref}
     className={cn(
-      "tailwind.config.jstop-full tailwind.config.jsz-[1] tailwind.config.jsflex tailwind.config.jsh-1.5 tailwind.config.jsitems-end tailwind.config.jsjustify-center tailwind.config.jsoverflow-hidden data-[state=visible]:tailwind.config.jsanimate-in data-[state=hidden]:tailwind.config.jsanimate-out data-[state=hidden]:tailwind.config.jsfade-out data-[state=visible]:tailwind.config.jsfade-in",
-      className
+      "top-full z-[1] flex h-1.5 items-end justify-center overflow-hidden data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out data-[state=visible]:fade-in",
+      className,
     )}
     {...props}
   >
-    <div className="tailwind.config.jsrelative tailwind.config.jstop-[60%] tailwind.config.jsh-2 tailwind.config.jsw-2 tailwind.config.jsrotate-45 tailwind.config.jsrounded-tl-sm tailwind.config.jsbg-border tailwind.config.jsshadow-md" />
+    <div className="relative top-[60%] h-2 w-2 rotate-45 rounded-tl-sm bg-border shadow-md" />
   </NavigationMenuPrimitive.Indicator>
-))
+));
 NavigationMenuIndicator.displayName =
-  NavigationMenuPrimitive.Indicator.displayName
+  NavigationMenuPrimitive.Indicator.displayName;
 
 export {
   navigationMenuTriggerStyle,
@@ -125,4 +125,4 @@ export {
   NavigationMenuLink,
   NavigationMenuIndicator,
   NavigationMenuViewport,
-}
+};
