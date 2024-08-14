4b5b5b40b45e07af650d2526acfbe9f648cf481f
naswa <endabelyuproject@gmail.com>
2024-08-14 16:02:07 +0700

    feat(formatter,gretter): create new function for greeting

---
+++ src/lib/helper/formatter.ts
@@ -0,0 +1,33 @@
+// formatDate.ts
+
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export function formatDate(date: Date): string {
+  const dayOfWeek = weekdays[date.getDay()];
+  const month = months[date.getMonth()];
+  const dayOfMonth = date.getDate();
+
+  return `${dayOfWeek}, ${month} ${dayOfMonth}`;
+}
---
+++ src/lib/helper/greeter.ts
@@ -0,0 +1,11 @@
+export function getGreeting(): string {
+  const hours = new Date().getHours();
+
+  if (hours < 12) {
+    return "Good morning";
+  } else if (hours < 18) {
+    return "Good afternoon";
+  } else {
+    return "Good evening";
+  }
+}
