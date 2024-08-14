f820faf439e7ed29185216777d06666e9de658c5
naswa <endabelyuproject@gmail.com>
2024-08-14 16:01:06 +0700

    feat(storage): add new function get workspace and task from localstorage

--- src/lib/helper/storage.ts
+++ src/lib/helper/storage.ts
@@ -1,4 +1,17 @@
+import { workspaceData } from "@/components/organism/baseLayout";
+import { Task, TaskList } from "@/models/task";
+
 export function getDataStorage(key: string) {
-  const data = JSON.parse(localStorage.getItem(key) || '[]');
+  const data = JSON.parse(localStorage.getItem(key) || "[]");
   return data;
 }
+
+export const getWorkspaces = (): workspaceData[] => {
+  const workspaces = localStorage.getItem("workspaces");
+  return workspaces ? JSON.parse(workspaces) : [];
+};
+
+export const getTasksForWorkspace = (workspaceId: string): TaskList[] => {
+  const tasks = localStorage.getItem(workspaceId);
+  return tasks ? JSON.parse(tasks) : [];
+};
