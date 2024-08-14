3df36e7ff7b8259a4b00b3e329f5e1eeeac24c9d
naswa <endabelyuproject@gmail.com>
2024-08-14 16:08:07 +0700

    feat(main): add router  using react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import BaseLayout from "./components/organism/baseLayout.tsx";
+import Home from "./pages/home.tsx";
+import { Toaster } from "./components/ui/toaster.tsx";
+import Workspace from "./pages/worskspace.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    element: <BaseLayout />,
+    children: [
+      { path: "home", element: <Home /> },
+      { path: "workspace", element: <Workspace /> },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    <Toaster />
   </React.StrictMode>,
-)
+);
